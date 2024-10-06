c9357c7442849ec26311ac33fdb959bd975c3b27
SantiagoSauma <lssaumap@gmail.com>
2024-10-05 19:47:57 -0600

    Fixed Bottom in Bar component

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/Bar.css';
 
 function Bar({ index, length, color, changeArray }) {
@@ -7,41 +7,6 @@ function Bar({ index, length, color, changeArray }) {
   useEffect(() => {
     setLen(length);
   }, [length]);
-
-  const colors = [
-    ['rgba(61, 90, 241, 0.5)', 'rgba(61, 90, 241, 0.2)'],
-    ['rgba(255, 48, 79, 1)', 'rgba(255, 48, 79, 0.5)'],
-    ['rgba(131, 232, 90, 0.5)', 'rgba(131, 232, 90, 0.2)'],
-  ];
-
-  const inputStyle = {
-    position: 'relative',
-    top: Math.floor(len / 2) - 12,
-    width: len,
-    left: -Math.floor(len / 2) + 13,
-    border: 'none',
-    background: 'none',
-  };
-
-  const bottom = {
-    transform: `translateY(${200 - len}px) rotateX(-90deg)`,
-    backgroundColor: `${colors[color][0]}`,
-    boxShadow: `5px 5px 50px 5px ${colors[color][1]}`,
-    transition: '0.3s',
-  };
-
-  const front_back_right_left = {
-    height: `${len}px`,
-    transform: `translateY(${200 - len}px)`,
-    backgroundColor: `${colors[color][0]}`,
-    boxShadow: `5px 5px 50px 5px ${colors[color][1]}`,
-    transition: '0.3s',
-  };
-
-  const quantity = {
-    position: 'relative',
-    top: 225,
-  };
 
   const handleChange = (e) => {
     let val = e.target.value;
@@ -75,52 +40,33 @@ function Bar({ index, length, color, changeArray }) {
   };
 
   return (
-    <>
-      <div className='bar'>
-        <div className='side top'></div>
-        <div className='side bottom' style={bottom}></div>
-        <div className='side right'>
-          <div
-            className='color-bar right-color-bar'
-            style={front_back_right_left}
-          ></div>
-        </div>
-        <div className='side left'>
-          <div
-            className='color-bar left-color-bar'
-            style={front_back_right_left}
-          ></div>
-        </div>
-        <div className='side front'>
-          <div
-            className='color-bar front-color-bar'
-            style={front_back_right_left}
-          >
-            <input
-              type='number'
-              style={inputStyle}
-              value={len}
-              className='input'
-              onChange={handleChange}
-            />
-          </div>
-        </div>
-        <div className='side back'>
-          <div
-            className='color-bar back-color-bar'
-            style={front_back_right_left}
-          ></div>
-        </div>
-        <div className='quantity-nav'>
-          <div className='quantity-button quantity-up' style={quantity} onClick={increment}>
-            +
-          </div>
-          <div className='quantity-button quantity-down' style={quantity} onClick={decrement}>
-            -
-          </div>
+    <div className={`bar color-${color}`} style={{ '--bar-height': `${len}px` }}>
+      <div className="side top"></div>
+      <div className="side bottom"></div>
+      <div className="side right">
+        <div className="color-bar right-color-bar"></div>
+      </div>
+      <div className="side left">
+        <div className="color-bar left-color-bar"></div>
+      </div>
+      <div className="side front">
+        <div className="color-bar front-color-bar">
+          <input
+            type="number"
+            value={len}
+            className="input"
+            onChange={handleChange}
+          />
         </div>
       </div>
-    </>
+      <div className="side back">
+        <div className="color-bar back-color-bar"></div>
+      </div>
+      <div className="quantity-nav">
+        <div className="quantity-button quantity-up" onClick={increment}>+</div>
+        <div className="quantity-button quantity-down" onClick={decrement}>-</div>
+      </div>
+    </div>
   );
 }
 
