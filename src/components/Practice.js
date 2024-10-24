a59b6ad81110b50893d6335c1201dc78681b0af7
SantiagoSauma <lssaumap@gmail.com>
2024-10-23 23:11:45 -0600

    "Coming Soon . . ." base page done

--- src/components/Practice.js
+++ src/components/Practice.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import TopBar from './TopBar';
 import './styles/Practice.css';
 
 function Practice() {
+  const navigate = useNavigate();
+
   return (
     <div className="practice-page">
       <TopBar />
       <div className="practice-content">
-        <h1>Practice - Exercises</h1>
+        <div className="header-row">
+          <button className="back-button" onClick={() => navigate(-1)}>
+            <img src={process.env.PUBLIC_URL + '/resources/arrow.png'} alt="Go Back" className="arrow-back" />
+          </button>
+          <h1>Practice - Exercises</h1>
+        </div>
         <div className="coming-soon-container">
-          <h2>Coming Soon...</h2>
+          <h2>Coming Soon . . .</h2>
         </div>
       </div>
     </div>
